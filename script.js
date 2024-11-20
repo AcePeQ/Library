e76d5301e8bc01bc9e653aeb8e33a2003397576b
AcePeQ <126714703+AcePeQ@users.noreply.github.com>
2024-11-20 10:09:06 +0100

    feat: add all styles and inject html to page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const libraryStorage = document.querySelector(".library_storage");
+
+console.log(libraryStorage);
+
 const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -13,12 +17,41 @@ function Book(title, author, pages, isRead) {
   };
 }
 
+function displayBooksInLibrary(library) {
+  console.log(library);
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
+    libraryStorage.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="book_container">
+            <div class="book">
+              <p class="book_name">${book.title}</p>
+              <p class="book_author">${book.author}</p>
+              <p class="book_pages">${book.pages} pages</p>
+              <p class="book_isRead">
+              ${
+                book.isRead ? "You read this book" : "You didn't read this book"
+              }
+              </p>
+              <div class="book_buttons">
+                <button class="button_book button-status">Change status</button>
+                <button class="button_book button-delete">Delete</button>
+              </div>
+            </div>
+          </div>
+      `
+    );
+  }
+}
+
 function addBookToLibrary(title, author, pages, isRead) {
   const newBook = new Book(title, author, pages, isRead);
 
   myLibrary.push(newBook);
 }
 
-console.log(myLibrary);
 addBookToLibrary("Hobbit", "J.K Rolings", 265, false);
-console.log(myLibrary);
+addBookToLibrary("Hobbit", "J.K Rolings", 265, false);
+
+displayBooksInLibrary(myLibrary);
